995d2c35ecb90481c619ea9f962f07576779414c
Jeyaraman <rjayavrp2@gmail.com>
2018-12-14 23:19:36 +0000

    Removed commented code

--- iotNode/src/iotState.cpp
+++ iotNode/src/iotState.cpp
@@ -85,60 +85,6 @@ class SENSE: public State
     void command(NodeManager *m)
     {
         cout << "\n   going from SENSE to COMMAND";
-        #if 0
-        if(m->t.status == SENSOR_ENABLE && m->h.status == SENSOR_ENABLE)
-        {
-          int cmd;
-          int readdata = 0;
-
-          while (1)
-          {
-              cout << "\nSensors status: Enabled \n";
-              cout << "1.Print Sensor Properties \n";
-              cout << "2.Read Temperature \n";
-              cout << "3.Read Humidity \n";
-              cout << "4.Print Data Queue(s) \n";
-              cout << "99.Exit \n";
-              cout << "Command[1-4]: ";
-              cin >> cmd;
-
-              if(cmd == 99)
-                break;
-            if(cmd >= 1 && cmd <= 4) {
-                switch(cmd)
-                {
-                case 1:
-                    m->t.printproperties();
-                    m->h.printproperties();
-                    break;
-                case 2:
-                    readdata = m->t.read();
-                    cout << "\n Temp Value:"<<readdata;
-                    m->temperature.qput(readdata);
-                    break;
-                case 3:
-                    readdata = m->h.read();
-                    cout << "\n Humidity Value:"<<readdata;
-                    m->humidity.qput(readdata);
-                    break;
-                case 4:
-                    cout << "\n Humidity Queue:";
-                    while((readdata=m->humidity.qget()) != 0)
-                          cout <<readdata<<"\t";
-
-                    cout << "\n Temp Queue:";
-                    while((readdata=m->temperature.qget()) != 0)
-                          cout <<readdata<<"\t";
-                    break;
-                }
-            }
-          }
-        }
-        else
-        {
-            cout << "\nSensors status: Disabled \n";
-        }
-        #endif
         m->setCurrent(new COMMAND());
         delete this;
     }
