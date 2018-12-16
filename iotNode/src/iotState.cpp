811ef785ecb5fa55f25064870798b35048d12c49
Jeyaraman <rjayavrp2@gmail.com>
2018-12-16 13:14:58 +0000

    Changes done for iotnode functionality invocation

--- iotNode/src/iotState.cpp
+++ iotNode/src/iotState.cpp
@@ -60,6 +60,63 @@ class COMMAND: public State
     {
         cout << "   dtor-COMMAND\n";
     };
+    COMMAND(NodeManager *nodemgr)
+    {
+        cout << "   COMMAND-ctor Processor";
+        if(nodemgr->t.status == SENSOR_ENABLE && nodemgr->h.status == SENSOR_ENABLE)
+        {
+          int cmd;
+          int readdata = 0;
+
+          while (1)
+          {
+              cout << "\nSensors status: Enabled \n";
+              cout << "1.Print Sensor Properties \n";
+              cout << "2.Read Temperature \n";
+              cout << "3.Read Humidity \n";
+              cout << "4.Print Data Queue(s) \n";
+              cout << "99.Exit \n";
+              cout << "Command[1-4]: ";
+              cin >> cmd;
+
+              if(cmd == 99)
+                break;
+              if(cmd >= 1 && cmd <= 4) {
+                switch(cmd)
+                {
+                case 1:
+                    nodemgr->t.printproperties();
+                    nodemgr->h.printproperties();
+                    break;
+                case 2:
+                    readdata = nodemgr->t.read();
+                    cout << "\n Temp Value:"<<readdata;
+                    nodemgr->temperature.qput(readdata);
+                    break;
+                case 3:
+                    readdata = nodemgr->h.read();
+                    cout << "\n Humidity Value:"<<readdata;
+                    nodemgr->humidity.qput(readdata);
+                    break;
+                case 4:
+                    cout << "\n Humidity Queue:";
+                    while((readdata=nodemgr->humidity.qget()) != 0)
+                          cout <<readdata<<"\t";
+
+                    cout << "\n Temp Queue:";
+                    while((readdata=nodemgr->temperature.qget()) != 0)
+                          cout <<readdata<<"\t";
+                    break;
+                }
+            }
+          }
+        }
+        else
+        {
+            cout << "\nSensors status: Disabled \n";
+        }
+
+    };
     void idle(NodeManager *m)
     {
         cout << "   going from COMMAND to IDLE";
@@ -82,10 +139,17 @@ class SENSE: public State
     {
         cout << "   dtor-SENSE\n";
     };
+    SENSE(NodeManager *nodemgr)
+    {
+        cout << "   SENSE-ctor Processor";
+        cout << "   Enabling Sensors... \n";
+        nodemgr->h.enabledisable(nodemgr->h.id, SENSOR_ENABLE);
+        nodemgr->t.enabledisable(nodemgr->t.id, SENSOR_ENABLE);
+    };
     void command(NodeManager *m)
     {
         cout << "\n   going from SENSE to COMMAND";
-        m->setCurrent(new COMMAND());
+        m->setCurrent(new COMMAND(m));
         delete this;
     }
 };
@@ -93,7 +157,7 @@ class SENSE: public State
 void IDLE::sense(NodeManager *m)
 {
   cout << "\n   going from IDLE to SENSE";
-  m->setCurrent(new SENSE());
+  m->setCurrent(new SENSE(m));
   delete this;
 }
 
--- iotNode/src/iotTempSensor.cpp
+++ iotNode/src/iotTempSensor.cpp
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-
 TempSensor::TempSensor()
 {
     //ctor
