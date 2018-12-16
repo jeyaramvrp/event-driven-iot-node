86f862a69c50fdb6bec8024ab4fd4e2d0fa9d8d0
Jeyaraman <rjayavrp2@gmail.com>
2018-12-16 13:16:40 +0000

    Changes done for iotnode functionality invocation

--- iotNode/main.cpp
+++ iotNode/main.cpp
@@ -16,72 +16,13 @@ int main()
   int num;
   while (1)
   {
-    cout << "Enter 0/1/2: ";
+    cout << "Enter 0/1/2/99: ";
     cin >> num;
     if(num >= 0 && num <= 2) {
         (nodemgr.*ptrs[num])();
-
-        if(num == 1) {
-            cout << "   Enabling Sensors... \n";
-            nodemgr.h.enabledisable(nodemgr.h.id, SENSOR_ENABLE);
-            nodemgr.t.enabledisable(nodemgr.t.id, SENSOR_ENABLE);
-        }
-        else if(num == 2){
-                if(nodemgr.t.status == SENSOR_ENABLE && nodemgr.h.status == SENSOR_ENABLE)
-        {
-          int cmd;
-          int readdata = 0;
-
-          while (1)
-          {
-              cout << "\nSensors status: Enabled \n";
-              cout << "1.Print Sensor Properties \n";
-              cout << "2.Read Temperature \n";
-              cout << "3.Read Humidity \n";
-              cout << "4.Print Data Queue(s) \n";
-              cout << "99.Exit \n";
-              cout << "Command[1-4]: ";
-              cin >> cmd;
-
-              if(cmd == 99)
-                break;
-            if(cmd >= 1 && cmd <= 4) {
-                switch(cmd)
-                {
-                case 1:
-                    nodemgr.t.printproperties();
-                    nodemgr.h.printproperties();
-                    break;
-                case 2:
-                    readdata = nodemgr.t.read();
-                    cout << "\n Temp Value:"<<readdata;
-                    nodemgr.temperature.qput(readdata);
-                    break;
-                case 3:
-                    readdata = nodemgr.h.read();
-                    cout << "\n Humidity Value:"<<readdata;
-                    nodemgr.humidity.qput(readdata);
-                    break;
-                case 4:
-                    cout << "\n Humidity Queue:";
-                    while((readdata=nodemgr.humidity.qget()) != 0)
-                          cout <<readdata<<"\t";
-
-                    cout << "\n Temp Queue:";
-                    while((readdata=nodemgr.temperature.qget()) != 0)
-                          cout <<readdata<<"\t";
-                    break;
-                }
-            }
-          }
-        }
-        else
-        {
-            cout << "\nSensors status: Disabled \n";
-        }
-        }
     }
+    else
+        break;
   }
-
   return 0;
 }
